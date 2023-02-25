5ae9fcd39fc2f1352aad900d815a2378b1b57b15
infernexio <sohail382517@gmail.com>
2023-02-25 11:27:05 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import dotenv
+import openai
+import os
+import sys
+
+dotenv.load_dotenv()
+
+prompt = sys.argv[1]
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+response = openai.Completion.create(
+    engine="text-davinci-003",
+    prompt=prompt,
+    max_tokens=1024,
+    n=1,
+    stop=None,
+    temperature=0.5,
+)
+
+message = response.choices[0].text
+print(message)
